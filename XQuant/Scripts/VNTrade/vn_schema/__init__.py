ff773db73fae17cf6b2616363734c11ac86e6d4c
Alkaid <kryuan@qq.com>
2023-10-28 10:13:19 +0800

    remove scripts

--- XQuant/Scripts/VNTrade/vn_schema/__init__.py
+++
@@ -1,8 +0,0 @@
-import json
-from pathlib import Path
-
-from .enums import *
-from .structs import *
-
-with open(Path(__file__).parent / "setting.json", encoding="utf-8") as fp:
-    setting = json.load(fp)
